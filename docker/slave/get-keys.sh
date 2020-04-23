a0c9accec84ca7d356dc279849381556d1dcdcd1
iscooter <scottccrosby@gmail.com>
2020-04-23 10:49:30 -0400

    use apt-key

--- docker/slave/get-keys.sh
+++ docker/slave/get-keys.sh
@@ -2,14 +2,16 @@
 n=0
 until [ $n -ge 5 ]
 do
-   gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "58118E89F3A912897C070ADBF76221572C52609D" && break
+   # gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "58118E89F3A912897C070ADBF76221572C52609D" && break
+   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "58118E89F3A912897C070ADBF76221572C52609D" && break
    n=$[$n+1]
    sleep 5
 done
 n=0
 until [ $n -ge 5 ]
 do
-   gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "7EA0A9C3F273FCD8" && break
+   # gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "7EA0A9C3F273FCD8" && break
+   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "7EA0A9C3F273FCD8" && break
    n=$[$n+1]
    sleep 5
 done
